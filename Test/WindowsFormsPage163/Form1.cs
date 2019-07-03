cd1c9d04bf269b2bd834a729abee0ca4714b5831
ftuczynski <ftuczynski@gmail.com>
2019-07-03 11:38:05 +0200

    zad ze str 163 skończone

--- Test/WindowsFormsPage163/Form1.cs
+++ Test/WindowsFormsPage163/Form1.cs
@@ -12,9 +12,43 @@ namespace WindowsFormsPage163
 {
     public partial class Form1 : Form
     {
+        Elephant lucinda;
+        Elephant lloyd;
+
         public Form1()
         {
             InitializeComponent();
+            lucinda = new Elephant() { Name = "Lucinda", EarSize = 33 };
+            lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+        }
+
+        //Lloyd button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lloyd.WhoAmI();
+        }
+
+        //Lucinda button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lucinda.WhoAmI();
+        }
+
+        //swap button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Elephant holder;
+            holder = lucinda;
+            lucinda = lloyd;
+            lloyd = holder;
+            MessageBox.Show("Swapped!");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            lloyd = lucinda;
+            lloyd.EarSize = 4321;
+            lloyd.WhoAmI();
         }
     }
 }
