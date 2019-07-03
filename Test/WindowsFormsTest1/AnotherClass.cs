b4223da81744df881ca4fc0c79ab6655ccc4676f
ftuczynski <ftuczynski@gmail.com>
2019-07-03 08:21:14 +0200

    Eksperymentowanie z Main()

---
+++ Test/WindowsFormsTest1/AnotherClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+namespace WindowsFormsTest1
+{
+    class AnotherClass
+    {
+        public static void NotMain()
+        {
+            MessageBox.Show("Pow!");
+            
+        }
+    }
+}
