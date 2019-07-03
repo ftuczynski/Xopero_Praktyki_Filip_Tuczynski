4b6fbaaaa9986c4a83213a384409b1182562585b
ftuczynski <ftuczynski@gmail.com>
2019-07-03 10:04:25 +0200

    Zadanie ze strony 129 zakończone

--- Test/WindowsFormsPage129/Form1.cs
+++ Test/WindowsFormsPage129/Form1.cs
@@ -12,9 +12,60 @@ namespace WindowsFormsPage129
 {
     public partial class Form1 : Form
     {
+        Guy joe;
+        Guy bob;
+        int bank = 100;
+
+        public void UpdateForm()
+        {
+            joesCashLabel.Text = joe.Name + " has $" + joe.Cash;
+            bobsCashLabel.Text = bob.Name + " has $" + bob.Cash;
+            bankCashLabel.Text = "The bank has $" + bank;
+        }
         public Form1()
         {
             InitializeComponent();
+
+            joe = new Guy() { Cash = 50, Name = "Joe" };
+
+            bob = new Guy() { Cash = 100, Name = "Bob" };
+
+            UpdateForm();
+        }
+
+        //Give $10 to Joe
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(bank >=10)
+            {
+                bank -= joe.ReceiveCash(10);
+                UpdateForm();
+            }
+            else
+            {
+                MessageBox.Show("The bank is out of money.");
+            }
+        }
+
+        //Receive $5 from Bob
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bank += bob.GiveCash(5);
+            UpdateForm();
+        }
+
+        //Joe gives $10 to Bob
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bob.ReceiveCash(joe.GiveCash(10));
+            UpdateForm();
+        }
+
+        //Bob gives $5 to Joe
+        private void button4_Click(object sender, EventArgs e)
+        {
+            joe.ReceiveCash(bob.GiveCash(5));
+            UpdateForm();
         }
     }
 }
--- Test/WindowsFormsPage129/Guy.cs
+++ Test/WindowsFormsPage129/Guy.cs
@@ -10,6 +10,35 @@ namespace WindowsFormsPage129
 {
     class Guy
     {
-        
+        public string Name;
+        public int Cash;
+
+        public int GiveCash(int amount)
+        {
+            if(amount<=Cash && amount>0)
+            {
+                Cash -= amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show("I don't have enough cash to give you " + amount, Name + "says...");
+                return 0;
+            }
+        }
+
+        public int ReceiveCash(int amount)
+        {
+            if(amount >0)
+            {
+                Cash += amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show(amount + " isn't an amount I'll take", Name + " says...");
+                return 0;
+            }
+        }
     }
 }
