d6d07fb9f5fa2ac383e1fc582cf430249b75fae7
ftuczynski <ftuczynski@gmail.com>
2019-07-08 15:53:56 +0200

    Dodano zad str 299

---
+++ Test/ConsolePage299/IStingPatrol.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePage299 {
+    interface IStingPatrol {
+        int AlertLevel { get; }
+        int StingerLength { get; }
+        bool LookForEnemies();
+        int SharpenStinger(int lenght);
+    }
+}
