28b7a9e03d9b295db3a41f47f599f43e91aa2a57
ftuczynski <ftuczynski@gmail.com>
2019-07-09 11:22:50 +0200

    skończono zad str 299

--- Test/ConsolePage299/Bee.cs
+++ Test/ConsolePage299/Bee.cs
@@ -6,6 +6,16 @@ using System.Threading.Tasks;
 
 namespace ConsolePage299 {
     class Bee : IStingPatrol {
+        public int AlertLevel { get; }
 
+        public int StingerLength { get; }
+
+        public bool LookForEnemies() {
+            return true;
+        }
+
+        public int SharpenStinger(int lenght) {
+            return 0;
+        }
     }
 }
