a1bc349779edbf8557184e94c7af9bad5843c048
ftuczynski <ftuczynski@gmail.com>
2019-07-10 15:45:20 +0200

    zad str 369

---
+++ Test/ConsolePage369/Duck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePage369 {
+    enum KindOfDuck {
+        Mallard,
+        Muscovy,
+        Decoy,
+    }
+
+    class Duck {
+        public int Size;
+        public KindOfDuck Kind;
+    }
+}
