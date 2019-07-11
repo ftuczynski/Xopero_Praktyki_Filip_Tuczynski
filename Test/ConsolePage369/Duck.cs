5daa5be829a9bc39864cbddee2782cff68072194
ftuczynski <ftuczynski@gmail.com>
2019-07-11 14:18:08 +0200

    skończono zad str 369

--- Test/ConsolePage369/Duck.cs
+++ Test/ConsolePage369/Duck.cs
@@ -11,8 +11,75 @@ namespace ConsolePage369 {
         Decoy,
     }
 
-    class Duck {
+    enum SortCriteria {
+        SizeThenKind,
+        KindThenSize,
+    }
+
+    class Duck : IComparable<Duck> {
         public int Size;
         public KindOfDuck Kind;
+
+        public int CompareTo(Duck duckToCompare) {
+            if (this.Size > duckToCompare.Size)
+                return 1;
+            else if (this.Size < duckToCompare.Size)
+                return -1;
+            else
+                return 0;
+        }
+    }
+
+    class DuckComparerBySize : IComparer<Duck> {
+        public int Compare(Duck x, Duck y) {
+            if (x.Size < y.Size)
+                return -1;
+            if (x.Size > y.Size)
+                return 1;
+            return 0;
+        }
+    }
+
+    class DuckComparerByKind : IComparer<Duck> {
+        public int Compare(Duck x, Duck y) {
+            if (x.Kind < y.Kind)
+                return -1;
+            if (x.Kind > y.Kind)
+                return 1;
+            return 0;
+        }
+    }
+
+    class DuckComparer : IComparer<Duck> {
+        public SortCriteria SortBy = SortCriteria.SizeThenKind;
+
+        public int Compare(Duck x, Duck y) {
+            if(SortBy == SortCriteria.SizeThenKind) {
+                if (x.Size > y.Size)
+                    return 1;
+                else if (x.Size < y.Size)
+                    return -1;
+                else {
+                    if (x.Kind > y.Kind)
+                        return 1;
+                    else if (x.Kind < y.Kind)
+                        return -1;
+                    return 0;
+                }
+            }
+            else {
+                if (x.Kind > y.Kind)
+                    return 1;
+                else if (x.Kind < y.Kind)
+                    return -1;
+                else {
+                    if (x.Size > y.Size)
+                        return 1;
+                    else if (x.Size < y.Size)
+                        return -1;
+                    return 0;
+                }
+            }
+        }
     }
 }
