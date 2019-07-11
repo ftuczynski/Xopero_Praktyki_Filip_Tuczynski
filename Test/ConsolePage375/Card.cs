9fa3562169af8e8e40c799e26d39d4b3ca4f7d29
ftuczynski <ftuczynski@gmail.com>
2019-07-11 15:57:54 +0200

    skończono zad str 375

---
+++ Test/ConsolePage375/Card.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePage375 {
+    class CardComparer_byValue : IComparer<Card> {
+        public int Compare(Card x, Card y) {
+            if (x.Value > y.Value)
+                return 1;
+            else if (x.Value < y.Value)
+                return -1;
+            return 0;
+        }
+    }
+
+    class Card {
+        public Suits Suit { get; set; }
+        public Values Value { get; set; }
+        public string Name {
+            get {
+                return Value.ToString() + " of " + Suit.ToString();
+            }
+        }
+
+        public Card(Suits suit,Values value) {
+            this.Suit = suit;
+            this.Value = value;
+        }
+    }
+}
---
+++ Test/ConsolePage375/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePage375 {
+    class Program {
+        static void Main(string[] args) {
+            List<Card> cards = new List<Card> {
+                new Card(Suits.Diamonds,Values.Ten),
+                new Card(Suits.Spades,Values.Queen),
+                new Card(Suits.Hearts,Values.Seven),
+                new Card(Suits.Hearts,Values.Jack),
+                new Card(Suits.Diamonds,Values.Queen)
+            };
+            CardComparer_byValue cardComparer = new CardComparer_byValue();
+            Console.WriteLine("Five random cards: ");
+            ReadCards(cards);
+            cards.Sort(cardComparer);
+            Console.WriteLine("\nThose same card, sorted: ");
+            ReadCards(cards);
+
+            Console.ReadKey();
+        }
+
+        public static void ReadCards(List<Card> cards) {
+            foreach(Card card in cards) {
+                Console.WriteLine(card.Name);
+            }
+        }
+    }
+}
