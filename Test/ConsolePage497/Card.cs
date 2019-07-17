7b3fc81e0624c5cb0236905c3903e803e76eb4ef
ftuczynski <ftuczynski@gmail.com>
2019-07-17 09:38:04 +0200

    skończono zad str 497

---
+++ Test/ConsolePage497/Card.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace ConsolePage497 {
+    class CardComparer_byValue : IComparer<Card> {
+        public int Compare(Card x, Card y) {
+            if (x.Value > y.Value)
+                return 1;
+            if (x.Value < y.Value)
+                return -1;
+            if (x.Suit > y.Suit)
+                return 1;
+            if (x.Suit < y.Suit)
+                return -1;
+            return 0;
+        }
+    }
+
+    class CardComparer_bySuit : IComparer<Card> {
+        public int Compare(Card x, Card y) {
+            if (x.Suit > y.Suit)
+                return 1;
+            if (x.Suit < y.Suit)
+                return -1;
+            if (x.Value > y.Value)
+                return 1;
+            if (x.Value < y.Value)
+                return -1;
+            return 0;
+        }
+    }
+
+    [Serializable]
+    class Card {
+        public Suits Suit { get; set; }
+        public Values Value { get; set; }
+        public string Name {
+            get {
+                return Value.ToString() + " of " + Suit.ToString();
+            }
+        }
+
+        public Card(Suits suit, Values value) {
+            this.Suit = suit;
+            this.Value = value;
+        }
+
+        public override string ToString() {
+            return Name;
+        }
+
+        public static bool DoesCardMatch(Card cardToCheck, Suits suit) {
+            if (cardToCheck.Suit == suit)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool DoesCardMatch(Card cardToCheck, Values value) {
+            if (cardToCheck.Value == value)
+                return true;
+            else
+                return false;
+        }
+
+        public static string Plural(Values value) {
+            if (value == Values.Six)
+                return "Sixes";
+            else
+                return value.ToString() + "s";
+        }
+    }
+}
---
+++ Test/ConsolePage497/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ConsolePage497 {
+    class Program {
+        static void Main(string[] args) {
+            Card ThreeOfClubs = new Card(Suits.Clubs, Values.Three);
+            Card SixOfHearts = new Card(Suits.Hearts, Values.Six);
+
+            using (Stream output = File.Create("three-c.dat")) {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(output, ThreeOfClubs);
+            }
+            using (Stream output = File.Create("six-h.dat")) {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(output, SixOfHearts);
+            }
+            byte[] firstFile = File.ReadAllBytes("three-c.dat");
+            byte[] secondFile = File.ReadAllBytes("six-h.dat");
+            for (int i = 0; i < firstFile.Length; i++) {
+                if (firstFile[i] != secondFile[i])
+                    Console.WriteLine("Byte #{0}: {1} versus {2}", i, firstFile[i], secondFile[i]);
+            }
+            firstFile[267] = (byte)Suits.Spades;
+            firstFile[316] = (byte)Values.King;
+            File.Delete("king-s.dat");
+            File.WriteAllBytes("king-s.dat", firstFile);
+
+            Card KingOfSpades;
+            using(Stream input = File.OpenRead("king-s.dat")) {
+                BinaryFormatter bf = new BinaryFormatter();
+                KingOfSpades = (Card)bf.Deserialize(input);
+            }
+            Console.WriteLine(KingOfSpades.Name);
+
+            Console.ReadKey();
+        }
+    }
+}
