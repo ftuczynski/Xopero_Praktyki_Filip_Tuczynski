df56e9b1bc8d9f16551777ba3154403d318abe30
ftuczynski <ftuczynski@gmail.com>
2019-07-19 11:42:28 +0200

    skończono WPF_GuySerializer

---
+++ Test/Wpf_GuySerializer/Card.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Wpf_GuySerializer {
+    enum Suits {
+        Spades,
+        Clubs,
+        Diamonds,
+        Hearts,
+    }
+
+    enum Values {
+        Ace = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13,
+    }
+    
+    [DataContract]
+    class Card {
+        [DataMember]
+        public Suits Suit { get; set; }
+
+        [DataMember]
+        public Values Value { get; set; }
+
+        public Card(Suits suit,Values value) {
+            this.Suit = suit;
+            this.Value = value;
+        }
+
+        private static Random r = new Random();
+
+        public static Card RandomCard() {
+            return new Card((Suits)r.Next(4), (Values)r.Next(1, 14));
+        }
+        public string Name {
+            get {
+                return Value.ToString() + " of " + Suit.ToString();
+            }
+        }
+        public override string ToString() {
+            return Name;
+        }
+    }
+}
---
+++ Test/Wpf_GuySerializer/GuyManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.IO;
+using System.ComponentModel;
+
+namespace Wpf_GuySerializer {
+    class GuyManager : INotifyPropertyChanged {
+        private Guy joe = new Guy("Joe", 37, 176.22M);
+        public Guy Joe { get { return joe; } }
+
+        private Guy bob = new Guy("Bob", 45, 4.68M);
+        public Guy Bob { get { return bob; } }
+
+        private Guy ed = new Guy("Ed", 43, 37.51M);
+        public Guy Ed { get { return ed; } }
+
+        public Guy NewGuy { get; set; }
+
+        public string GuyFile { get; set; }
+
+        public void ReadGuy() {
+            if (String.IsNullOrEmpty(GuyFile))
+                return;
+
+            using(Stream inputStream = File.OpenRead(GuyFile)) {
+                DataContractSerializer serializer = new DataContractSerializer(typeof(Guy));
+                NewGuy = serializer.ReadObject(inputStream) as Guy;
+            }
+            OnPropertyChanged("NewGuy");
+        }
+
+        public void WriteGuy(Guy guyToWrite) {
+            GuyFile = Path.GetFullPath(guyToWrite.Name + ".xml");
+
+            if (File.Exists(GuyFile))
+                File.Delete(GuyFile);
+            using(Stream OutputStream = File.OpenWrite(GuyFile)) {
+                DataContractSerializer serializer = new DataContractSerializer(typeof(Guy));
+                serializer.WriteObject(OutputStream, guyToWrite);
+            }
+            OnPropertyChanged("GuyFile");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
