093ba19a9f7c6d0e8149494999c16c077ffae9b2
ftuczynski <ftuczynski@gmail.com>
2019-07-22 15:52:25 +0200

    skończono zad str 629

---
+++ Test/ConsolePage629/Dog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePage629 {
+    public struct Dog {
+        public string Name;
+        public string Breed;
+
+        public Dog(string name,string breed) {
+            this.Name = name;
+            this.Breed = breed;
+        }
+
+        public void Speak() {
+            Console.WriteLine("My name is {0} and I'm a {1}.", Name, Breed);
+        }
+    }
+
+    public class Canine {
+        public string Name;
+        public string Breed;
+
+        public Canine(string name, string breed) {
+            this.Name = name;
+            this.Breed = breed;
+        }
+
+        public void Speak() {
+            Console.WriteLine("My name is {0} and I'm a {1}.", Name, Breed);
+        }
+    }
+}
---
+++ Test/ConsolePage629/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsolePage629 {
+    class Program {
+        static void Main(string[] args) {
+            //class
+            Canine spot = new Canine("Spot", "pug");
+            Canine bob = spot;
+            bob.Name = "Spike";
+            bob.Breed = "beagle";
+            spot.Speak();
+            //structure
+            Dog jake = new Dog("Jake", "poodle");
+            Dog betty = jake;
+            betty.Name = "Betty";
+            betty.Breed = "pit bull";
+            jake.Speak();
+
+            Console.ReadKey();
+        }
+    }
+}
