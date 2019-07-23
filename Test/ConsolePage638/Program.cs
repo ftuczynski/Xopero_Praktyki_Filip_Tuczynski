064b67a5ba799066d3923e713e1290ceb12b8354
ftuczynski <ftuczynski@gmail.com>
2019-07-23 11:27:11 +0200

    skończono zad str 629

---
+++ Test/ConsolePage638/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsolePage638 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Enter birthday: ");
+            string birthday = Console.ReadLine();
+            Console.Write("Enter height in inches: ");
+            string height = Console.ReadLine();
+            RobustGuy guy = new RobustGuy(birthday, height);
+            Console.WriteLine(guy.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
