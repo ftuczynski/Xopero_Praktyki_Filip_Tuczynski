64b65eb551ccab50724ea539ddc92b407a4ebdb9
ftuczynski <ftuczynski@gmail.com>
2019-07-24 12:10:48 +0200

    skończono zad str 732

---
+++ Test/WindowsFormsPage732/Amy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsPage732 {
+    class Amy {
+        public GetSecretIngredient AmysSecretIngredientMethod {
+            get {
+                return new GetSecretIngredient(AmySecretIngredient);
+            }
+        }
+        private string AmySecretIngredient(int amount) {
+            if (amount < 10)
+                return amount.ToString() + " cans of sardines -- you need more!";
+            else
+                return amount.ToString() + " cans of sardines";
+        }
+    }
+}
