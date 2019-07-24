b84d669eea0a612490e8746a8962948cd28862f1
ftuczynski <ftuczynski@gmail.com>
2019-07-24 15:14:35 +0200

    skończono zad Wpf_BasketballRoster str 750

---
+++ Test/Wpf_BasketballRoster/ViewModel/PlayerViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Wpf_BasketballRoster.ViewModel {
+    class PlayerViewModel {
+        public string Name { get; set; }
+        public int Number { get; set; }
+
+        public PlayerViewModel(string name, int number) {
+            Name = name;
+            Number = number;
+        }
+    }
+}
---
+++ Test/Wpf_BasketballRoster/ViewModel/RosterViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Collections.ObjectModel;
+using Wpf_BasketballRoster.Model;
+
+namespace Wpf_BasketballRoster.ViewModel {
+    class RosterViewModel {
+        public ObservableCollection<PlayerViewModel> Starters { get; set; }
+        public ObservableCollection<PlayerViewModel> Bench { get; set; }
+        private Roster _roster;
+        private string _teamName;
+        public string TeamName {
+            get { return _teamName; }
+            set {
+                _teamName = value;
+            }
+        }
+
+        public RosterViewModel(Roster roster) {
+            _roster = roster;
+            Starters = new ObservableCollection<PlayerViewModel>();
+            Bench = new ObservableCollection<PlayerViewModel>();
+
+            TeamName = _roster.TeamName;
+
+            UpdateRosters();
+        }
+
+        private void UpdateRosters() {
+            var startingPlayers =
+                from player in _roster.Players
+                where player.Starter
+                select player;
+
+            foreach (Player player in startingPlayers)
+                Starters.Add(new PlayerViewModel(player.Name, player.Number));
+
+            var benchPlayers =
+                from player in _roster.Players
+                where player.Starter == false
+                select player;
+
+            foreach (Player player in benchPlayers)
+                Bench.Add(new PlayerViewModel(player.Name, player.Number));
+        }
+    }
+}
