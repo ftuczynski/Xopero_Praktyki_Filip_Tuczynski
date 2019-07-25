7797945047acea730610cfc64f25a6de1cadfa88
ftuczynski <ftuczynski@gmail.com>
2019-07-25 15:38:51 +0200

    rozpocząto zad Wpf_AnimatedBee str 790

---
+++ Test/Wpf_AnimatedBee/View/FlyingBees.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace Wpf_AnimatedBee.View {
+    /// <summary>
+    /// Interaction logic for FlyingBees.xaml
+    /// </summary>
+    public partial class FlyingBees : Window {
+        public FlyingBees() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Test/Wpf_AnimatedBee/ViewModel/BeeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wpf_AnimatedBee.View;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Wpf_AnimatedBee.ViewModel {
+    class BeeViewModel {
+        private readonly ObservableCollection<System.Windows.UIElement> _sprites = new ObservableCollection<System.Windows.UIElement>();
+
+    }
+}
