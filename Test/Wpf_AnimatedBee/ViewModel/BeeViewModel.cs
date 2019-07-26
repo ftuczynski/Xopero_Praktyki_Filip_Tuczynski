67990dfbeba1945213b8434b0f314ea649f966b7
ftuczynski <ftuczynski@gmail.com>
2019-07-26 08:44:40 +0200

    skończono Wpf_AnimatedBee

--- Test/Wpf_AnimatedBee/ViewModel/BeeViewModel.cs
+++ Test/Wpf_AnimatedBee/ViewModel/BeeViewModel.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Wpf_AnimatedBee.View;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
@@ -10,6 +6,19 @@ using System.Collections.Specialized;
 namespace Wpf_AnimatedBee.ViewModel {
     class BeeViewModel {
         private readonly ObservableCollection<System.Windows.UIElement> _sprites = new ObservableCollection<System.Windows.UIElement>();
+        public INotifyCollectionChanged Sprites { get { return _sprites; } }
 
+        public BeeViewModel() {
+            AnimatedImage firstBee = BeeHelper.BeeFactory(50, 50, TimeSpan.FromMilliseconds(50));
+            _sprites.Add(firstBee);
+            AnimatedImage secondBee = BeeHelper.BeeFactory(200, 200, TimeSpan.FromMilliseconds(10));
+            _sprites.Add(secondBee);
+            AnimatedImage thirdBee = BeeHelper.BeeFactory(300, 125, TimeSpan.FromMilliseconds(100));
+            _sprites.Add(thirdBee);
+
+            BeeHelper.MakeBeeMove(firstBee, 50, 450, 40);
+            BeeHelper.SetBeeLocation(secondBee, 80, 260);
+            BeeHelper.SetBeeLocation(thirdBee, 230, 100);
+        }
     }
 }
